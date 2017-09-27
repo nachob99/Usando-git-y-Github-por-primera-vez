b75e8191918e2bbc672b9f425b257eda6db89d1d
nachob99 <nachoarriola99@gmail.com>
2017-09-27 12:52:42 +0200

    Mes actual

--- ejemplo.java
+++ ejemplo.java
@@ -2,8 +2,8 @@ public class ejemplo{
  public static void main(String args[]){
 
 	System.out.println("NACHO BARRIOLUENGO");
-	System.out.println("17");
+	System.out.println("18");
 	System.out.println("León");
-
+	System.out.println("Septiembre");
  }
 }
